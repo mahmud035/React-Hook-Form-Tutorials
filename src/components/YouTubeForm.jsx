576d06bf1d32a6a62889e9241e4f22d2046f3c34
mahmud035 <mahamudulhasan4148@gmail.com>
2023-04-19 06:44:08 +0600

    03: managing form state: register

--- src/components/YouTubeForm.jsx
+++ src/components/YouTubeForm.jsx
@@ -1,19 +1,58 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const YouTubeForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  console.log(errors);
+
+  const handleRegister = (data) => {
+    console.log(data);
+
+    const username = data.username;
+    const email = data.email;
+    const channel = data.channel;
+    // console.log(username, email, channel);
+  };
+
   return (
     <div>
       <h1>YouTube Form</h1>
 
-      <form>
+      <form onSubmit={handleSubmit(handleRegister)}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" />
+        <input
+          {...register('username', { required: true })}
+          type="text"
+          id="username"
+          name="username"
+        />
+
+        {errors?.username && <span>This field is required</span>}
 
         <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" name="email" />
+        <input
+          {...register('email', { required: true })}
+          type="email"
+          id="email"
+          name="email"
+        />
+
+        {errors?.email && <span>This field is required</span>}
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" name="channel" />
+        <input
+          {...register('channel', { required: true })}
+          type="text"
+          id="channel"
+          name="channel"
+        />
+
+        {errors.channel && <span>This field is required</span>}
 
         <button>Submit</button>
       </form>
